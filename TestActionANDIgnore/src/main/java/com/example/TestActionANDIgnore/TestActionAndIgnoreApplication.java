30d1d86e7d4c3cebd65ea7d255418449d08d16d4
Minwooooooo <110237141+Minwooooooo@users.noreply.github.com>
2022-09-08 12:57:55 +0900

    Add files via upload

---
+++ TestActionANDIgnore/src/main/java/com/example/TestActionANDIgnore/TestActionAndIgnoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.TestActionANDIgnore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestActionAndIgnoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestActionAndIgnoreApplication.class, args);
+	}
+
+}
---
+++ TestActionANDIgnore/src/test/java/com/example/TestActionANDIgnore/TestActionAndIgnoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TestActionANDIgnore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestActionAndIgnoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
