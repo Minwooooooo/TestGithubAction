b776364d9392a7ec35e6d6e68bd1f9ca3aa22572
Minwooooooo <achilleus_@nate.com>
2022-09-08 20:50:27 +0900

    파일 디렉토리 수정

--- src/main/java/com/example/TestActionANDIgnore/service/LottoService.java
+++ src/main/java/com/example/TestActionANDIgnore/service/LottoService.java
@@ -10,7 +10,6 @@ import com.example.TestActionANDIgnore.repo.LottoRepository;
 import com.example.TestActionANDIgnore.repo.LottoServerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
